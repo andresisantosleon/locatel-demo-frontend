f9d0ab79f0c216ddf634339f5108ff6c858f0628
aisantosl <aisantosl@pop-os.localdomain>
2024-11-18 07:48:21 -0500

    draft for sale

---
+++ src/components/ProductInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      v-model="Product.ID" 
+      @input="onInputEdited" 
+      placeholder="Ingrese ID de producto" 
+    />
+    <input 
+      type="number" 
+      v-model="Product.quantity" 
+      @input="onInputEdited" 
+      placeholder="Ingrese cantidad" 
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue'
+
+const emit = defineEmits(['input-edited'])
+const Product = ref({ID: '',quantity: ''})
+
+const onInputEdited = () => {
+  if (Product.value.ID === '' || Product.value.quantity === '') return
+  emit('input-edited', Product.value)
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <ProductInput v-for="(product, index) in products" :key="index" @input-edited="onInputEdited" />
+    <button @click="()=>(products.push(1))">Agregar producto</button>
   </main>
+  {{ productsData }}
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import ProductInput from '../components/ProductInput.vue'
+
+const products = ref([1]);
+const productsData = ref([]);
+
+const onInputEdited = (product) => {
+  productsData.value.push(product);
+}
+</script>
